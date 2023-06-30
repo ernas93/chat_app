d87a2ffced985217b701c992954120568a94435c
ernas93 <107477572+ernas93@users.noreply.github.com>
2023-06-30 23:05:14 +0200

    adding firestore

--- App.js
+++ App.js
@@ -9,7 +9,26 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 //creating the navigator
 const Stack = createNativeStackNavigator();
 
+//importing firestore databe
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+
 const App = () => {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC4HuOfha7pqdvoRohfOikwvYbY8fqO59g",
+    authDomain: "chat-app-69a66.firebaseapp.com",
+    projectId: "chat-app-69a66",
+    storageBucket: "chat-app-69a66.appspot.com",
+    messagingSenderId: "680772181777",
+    appId: "1:680772181777:web:799e2748932520c2fbb6b0"
+  };
+  // initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  // initialize Clou Firestore and get a reference to the service
+  const db = getFirestore(app);
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -22,7 +41,9 @@ const App = () => {
         <Stack.Screen
           name="Chat"
           component={Chat}
-        />
+        >
+          {props => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   )
